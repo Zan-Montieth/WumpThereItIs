56d8278b6598c979e7fbf5a3a6c807f81d321174
Zan Montieth <zan.montieth@gmail.com>
2018-11-18 18:54:48 -0700

    adding functionality

--- src/Node.java
+++ src/Node.java
@@ -5,13 +5,20 @@ public class Node {
     private boolean pit = false;
     private boolean breeze = false;
     private boolean gold = false;
+    private boolean glitter = false;
     private boolean wumpus = false;
+    private boolean stench = false;
+    private boolean player = false;
 
 
     public Node(int inX, int inY){
         xCoord = inX;
         yCoord = inY;
     }
+
+    public void setPlayer(boolean in){
+        player = in;
+    };
 
     public void setStart(){
         start = true;
@@ -36,4 +43,52 @@ public class Node {
     public void setStench(){
         stench = true;
     }
+
+    public void setGlitter(){
+        glitter = true;
+    }
+
+    public boolean isBreeze() {
+        return breeze;
+    }
+
+    public boolean isGold() {
+        return gold;
+    }
+
+    public boolean isPit() {
+        return pit;
+    }
+
+    public boolean isStart() {
+        return start;
+    }
+
+    public boolean isStench() {
+        return stench;
+    }
+
+    public boolean isWumpus() {
+        return wumpus;
+    }
+
+    public boolean isGlitter() {
+        return glitter;
+    }
+
+    public boolean isPlayer(){
+        return player;
+    }
+
+    public int getxCoord(){
+        return xCoord;
+    }
+
+    public int getyCoord() {
+        return yCoord;
+    }
+
+
+
+
 }
